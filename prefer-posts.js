778d62135da84a23d1f179eb98b403025d658b1a
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-08-06 15:17:43 +0200

    category alert

--- prefer-posts.js
+++ prefer-posts.js
@@ -1,46 +1,80 @@
 class CategoryPosts extends HTMLElement {
-
-
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this.categoryArray = ['gaming', 'history', 'animals_and_pets', 'movies', 'science', 'food_and_drink', 'travel', 'music', 'programming', 'hobbies']
-
+        this.categoryArray = ['gaming', 'history', 'animals_and_pets', 'movies', 'science', 'food_and_drink', 'travel', 'music', 'programming', 'hobbies'];
+        this.selectedCategories = new Set();
+        this.posts = [];
     }
 
     connectedCallback() {
-     
-
-    this.render(this.categoryArray);
-        
-
+        this.render();
+        this.loadPosts();
     }
 
+    loadPosts() {
+        const promises = this.categoryArray.map((category) =>
+            fetch('https://www.reddit.com/r/' + category + '/new.json').then((resp) => resp.json())
+        );
 
-
-
+        Promise.all(promises)
+            .then((responses) => {
+                responses.forEach((res) => {
+                    if (res.data && res.data.children) {
+                        const categoryPosts = res.data.children.map((post) => post.data);
+                        this.posts = [...this.posts, ...categoryPosts];
+                    }
+                });
+                this.showFilteredPosts();
+            })
+            .catch((error) => {
+                console.error('Error fetching posts:', error);
+            });
+    }
 
     render() {
+        this.shadowRoot.innerHTML = '';
 
-        const dialog = document.getElementById('dialog')
-        const dialogInput = document.createElement('div')
+        const mainContainer = document.createElement('div');
+        this.shadowRoot.appendChild(mainContainer);
+
+        const dialog = document.getElementById('dialog');
+        const dialogInput = document.createElement('div');
 
         for (let i = 0; i < this.categoryArray.length; i++) {
             const input = this.categoryArray[i];
+            dialogInput.innerHTML += `<input type="checkbox" name="${input}" value="${input}" id="${input}"> <label for="${input}">${input}</label>`;
+        }
 
-            dialogInput.innerHTML += `<input type="checkbox" name="${input}" value="${input}" id=""> <label for="${input}">${input}</label>`
+        const okButton = document.getElementById('okButton');
+        okButton.addEventListener('click', () => {
+            this.selectedCategories.clear();
+            const checkboxes = dialog.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach((checkbox) => {
+                if (checkbox.checked) {
+                    this.selectedCategories.add(checkbox.value);
+                }
+            });
+            this.showFilteredPosts();
+            dialog.style.display = 'none';
+        });
 
-          
-
-        } dialog.appendChild(dialogInput)
-        } 
-
+        dialogInput.appendChild(okButton);
+        dialog.appendChild(dialogInput);
     }
 
+    showFilteredPosts() {
+        const filteredPosts = this.posts.filter((post) => this.selectedCategories.has(post.category));
 
+        const postContainer = document.getElementById('postContainer');
+        postContainer.innerHTML = '';
 
-
-
-
+        filteredPosts.forEach((post) => {
+            const cardComponent = document.createElement('post-card');
+            cardComponent.post = post;
+            postContainer.appendChild(cardComponent);
+        });
+    }
+}
 
 customElements.define('pref-dialog', CategoryPosts);
--- reddit-post.js
+++ reddit-post.js
@@ -1,5 +1,4 @@
 class PostCardComponent extends HTMLElement {
-
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
@@ -13,31 +12,28 @@ class PostCardComponent extends HTMLElement {
         if (this.post) {
             this.shadowRoot.innerHTML = '';
 
-            this.shadowRoot.innerHTML = `
-            <div class="card-post"> 
-    <div class="card-header"> 
-        <span class="span-created">${toHumanTime(this.post.data.created)} </span>
-        <div class="h3-title"> 
-            <h3> ${this.post.data.title}</h3>
-        </div>
-        <div class="h3-author"> 
-            <h3>${this.post.data.author_fullname} </h3>
-        </div>
-        <div class="img-container"> <img src="${this.post.data.thumbnail} " alt=""> </div>
-        <div class="details"> 
-            <a href="${this.post.data.url}" target="_blank" rel="noopener noreferrer"></a>
-        </div>
+            this.shadowRoot.innerHTML += `<style>
+               
+            </style>`;
 
-        
-    </div>
-</div>
-             
-             `
-           
-            // const deleteBtn = document.createElement('button');
-            // deleteBtn.textContent = 'cancellami';
-            // div.appendChild(deleteBtn);
-            // deleteBtn.addEventListener('click', () => this.emitEvent());
+            this.shadowRoot.innerHTML += `
+                <div class="card-post">
+                    <div class="card-header">
+                        <span class="span-created">${toHumanTime(this.post.data.created)}</span>
+                        <div class="h3-title">
+                            <h3>${this.post.data.title}</h3>
+                        </div>
+                        <div class="h3-author">
+                            <h3>${this.post.data.author_fullname}</h3>
+                        </div>
+                        <div class="img-container">
+                            <img src="${this.post.data.thumbnail}" alt="">
+                        </div>
+                        <div class="details">
+                            <a href="${this.post.data.url}" target="_blank" rel="noopener noreferrer"></a>
+                        </div>
+                    </div>
+                </div>`;
         }
     }
 
@@ -48,30 +44,27 @@ class PostCardComponent extends HTMLElement {
 }
 
 function toHumanTime(timestamp) {
-   
+    const timestampInMils = timestamp * 1000;
+    const now = Date.now();
 
-    const timestampInMils = timestamp * 1000
-    const now = Date.now()
+    const delta = now - timestampInMils;
 
-    const delta = now - timestampInMils
-
-    let second = parseInt(delta / 1000)
-    if(second < 60){
-        return second + ' secondi fa'
+    let second = parseInt(delta / 1000);
+    if (second < 60) {
+        return second + ' secondi fa';
     }
-    
-    let minuti = parseInt(second / 60)
-    if(minuti < 60){
-        return minuti + ' minuti fa'
+
+    let minuti = parseInt(second / 60);
+    if (minuti < 60) {
+        return minuti + ' minuti fa';
     }
 
     let ore = parseInt((minuti / 60));
-    if(ore < 24){
-        return ore + ' ore fa'
-    } 
-   
-    return parseInt(ore / 24) + ' giorni fa'
-   
+    if (ore < 24) {
+        return ore + ' ore fa';
+    }
+
+    return parseInt(ore / 24) + ' giorni fa';
 }
 
 customElements.define('post-card', PostCardComponent);
