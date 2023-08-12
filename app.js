a4783b8b7043658ec22cbf8f12ddbb1422fab72f
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-08-12 10:33:12 +0200

    up

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
 
 
+// const nextBtn = document.querySelector('#next-page');
+//          nextBtn.addEventListener('click', () => {
+//           console.log('avanti')
+//             for (const category of selectedCategories()) {
+               
+//                 fetch(`https://www.reddit.com/r/${category}/hot/.json?after=${this.afterId}`)
+//                     .then(resp => resp.json())
+//                     .then(data => {
+//                         if (data.data && data.data.children) {
+//                             for (const postData of data.data.children) {
+//                                 this.posts.push(postData.data);
+//                             }
+//                         }
+//                         this.showFilteredPosts();
+//                     })
+//                     .catch(error => {
+//                         console.error(`Error fetching posts for ${category}:`, error);
+//                     });
+//             }
+//         });
+
+//         const previousBtn = document.querySelector('#previous-page');
+//          previousBtn.addEventListener('click', () => {
+
+//             console.log('indietro ')
+          
+//             for (const category of selectedCategories) {
+               
+//                 fetch(`https://www.reddit.com/r/${category}/hot/.json?before=${this.afterId}`)
+//                     .then(resp => resp.json())
+//                     .then(data => {
+//                         if (data.data && data.data.children) {
+//                             for (const postData of data.data.children) {
+//                                 this.posts.push(postData.data);
+//                             }
+//                         }
+//                         this.showFilteredPosts();
+//                     })
+//                     .catch(error => {
+//                         console.error(`Error fetching posts for ${category}:`, error);
+//                     });
+//             }
+//         });
--- web-components/prefer-posts.js
+++ web-components/prefer-posts.js
@@ -53,49 +53,7 @@ class CategoryPosts extends HTMLElement {
     render() {
         this.shadowRoot.innerHTML = '';
 
-        const nextBtn = this.shadowRoot.getElementById('next-page');
-         nextBtn.addEventListener('click', () => {
-          console.log('avanti')
-            for (const category of this.selectedCategories) {
-               
-                fetch(`https://www.reddit.com/r/${category}/hot/.json?after=${this.afterId}`)
-                    .then(resp => resp.json())
-                    .then(data => {
-                        if (data.data && data.data.children) {
-                            for (const postData of data.data.children) {
-                                this.posts.push(postData.data);
-                            }
-                        }
-                        this.showFilteredPosts();
-                    })
-                    .catch(error => {
-                        console.error(`Error fetching posts for ${category}:`, error);
-                    });
-            }
-        });
-
-        const previousBtn = this.shadowRoot.getElementById('previous-page');
-         previousBtn.addEventListener('click', () => {
-
-            console.log('indietro ')
-          
-            for (const category of this.selectedCategories) {
-               
-                fetch(`https://www.reddit.com/r/${category}/hot/.json?before=${this.afterId}`)
-                    .then(resp => resp.json())
-                    .then(data => {
-                        if (data.data && data.data.children) {
-                            for (const postData of data.data.children) {
-                                this.posts.push(postData.data);
-                            }
-                        }
-                        this.showFilteredPosts();
-                    })
-                    .catch(error => {
-                        console.error(`Error fetching posts for ${category}:`, error);
-                    });
-            }
-        });
+        
 
 
         const dialog = document.getElementById('dialog-container');
